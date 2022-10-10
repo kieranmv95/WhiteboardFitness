4a64771d81ddf97c7b2e36494a04073d338b9df5
Kieran Venison <kieranvenison@Kierans-MacBook-Pro.local>
2022-10-10 20:57:15 +0100

    feat: added footer links

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,10 +1,33 @@
 import * as S from "./style";
 import * as PageStyles from "../../styles/pages/shared";
+import Link from "next/link";
 
 const Footer = () => (
   <S.Footer>
     <PageStyles.Container>
       <S.Brand>Whiteboard Fitness</S.Brand>
+      <S.Links>
+        <li>
+          <Link href="/blog">
+            <S.Link>Blog</S.Link>
+          </Link>
+        </li>
+        <li>
+          <Link href="/workouts">
+            <S.Link>Workouts</S.Link>
+          </Link>
+        </li>
+        <li>
+          <Link href="/tools">
+            <S.Link>Tools</S.Link>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <S.Link>About</S.Link>
+          </Link>
+        </li>
+      </S.Links>
     </PageStyles.Container>
   </S.Footer>
 );
--- components/Footer/style.ts
+++ components/Footer/style.ts
@@ -9,3 +9,15 @@ export const Footer = styled.footer`
 export const Brand = styled.h3`
   margin: 0;
 `;
+
+export const Links = styled.ul`
+  list-style-type: none;
+  margin: 1 0 0 0;
+  padding: 0;
+`;
+
+export const Link = styled.a`
+  color: ${({ theme }) => theme.colors.bg};
+  display: block;
+  padding: 0.1rem 0;
+`;
